32a5ab80bcdfec6a51b5b053e6c276288c5f5c6a
zigfrei <zigfrei.zf@gmail.com>
2023-09-26 11:20:01 +0300

    feat: add store

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,19 @@
-import Header from '../Header/Header';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import Header from "../Header/Header";
+import { getUser } from "../../services/slices/userSlice";
 
 function Main() {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    return (
-        <Header />
-    )
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  console.log(user);
+
+  return <Header />;
 }
 
-export default Main
+export default Main;
