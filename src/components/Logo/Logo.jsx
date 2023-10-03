9808a119eb40bf3bdcd10d1c21bd3eaef6ae36cf
zigfrei <zigfrei.zf@gmail.com>
2023-10-03 22:49:55 +0300

    hotfix: add vite-plugin-svgr to import svg as ReactComponent

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-  import style from './Logo.module.scss';
+import style from './Logo.module.scss';
+import LogoImg from '../../images/logo.svg?react';
 
-  function Logo() {
-    return (
-      <div className={style.logo_container}>
-        <img src='../../images/logo.svg' className={style.logo__image} alt='Logo image'/>
-      </div>
-    );
-  }
-  
-  export default Logo;
+function Logo() {
+  return (
+    <div className={style.logo_container}>
+      <LogoImg />
+    </div>
+  );
+}
+
+export default Logo;
