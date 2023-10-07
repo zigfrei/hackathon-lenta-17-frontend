ff2d24a3e2f815adb6e700dec828eb0550785cf2
Katya Aslanidi <grechkakat@yandex.ru>
2023-10-07 21:39:24 +0300

    feat: add dropdown

--- src/components/DropdownItem/DropdownItem.jsx
+++ src/components/DropdownItem/DropdownItem.jsx
@@ -1,17 +1,31 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import style from './DropdownItem.module.scss';
 
-export function DropdownItem() {
+export function DropdownItem({ selected, setSelected }) {
 
     const [isActive, setIsActive] = useState(false);
-
+    const options = ["1", "2", "3"];
 
     return (
         <div>
-            <div>
-                Choose one
+            <div onClick={(e) => setIsActive(!isActive)}>
+                {selected}
                 <span></span>
             </div>
+            {isActive && (
+                <div>
+                    {options.map((option) => {
+                        <div
+                            onClick={(e) => {
+                                setSelected(option);
+                                setIsActive(false);
+                            }}
+                        >
+                            {option}
+                        </div>
+                    })}
+                </div>
+            )}
         </div>
     );
 }
